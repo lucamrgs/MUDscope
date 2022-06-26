a690e26f3ea2795d95c44d85438cafede71ca830
lucamrgs <luca.morgese@studenti.unitn.it>
2022-06-26 18:08:48 +0200

    moved away pending code for attacks

--- attacks_apis/noise.py
+++
@@ -1,5 +0,0 @@
-
-"""
-    Ref: https://www.tutorialspoint.com/python_penetration_testing/python_penetration_testing_dos_and_ddos_attack.htm
-    Take inspiration and generate random-ish noise-like (e.g., RST backscatter) packets at given rate.
-"""
