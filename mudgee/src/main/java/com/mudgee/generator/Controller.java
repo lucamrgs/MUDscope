9806e6ad098a154434d3666a624da759379dc802
Thijs van Ede <t.s.vanede@utwente.nl>
2022-09-12 13:35:04 +0200

    Removed mudgee from repo, this is a dependency and should be installed separately

--- mudgee/src/main/java/com/mudgee/generator/Controller.java
+++
@@ -1,9 +0,0 @@
-package com.mudgee.generator;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class Controller {
-
-	public static final Map<String, String> controllerMap = new HashMap<>();
-}
