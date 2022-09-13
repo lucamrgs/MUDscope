d73bd5eb7c20fedc0d72748c35851dd5c3e6e2b4
Thijs van Ede <t.s.vanede@utwente.nl>
2022-09-13 10:36:43 +0200

    Cleaned main usage

--- examples/example.sh
+++ examples/example.sh
@@ -17,11 +17,11 @@ mkdir result
 # 1. Create MUD profile from benign data traces
 # UT trace
 python3 -m mudscope mudgen \
-    --mudgen_config config/mudgen/ut-tplink.json
+    --config config/mudgen/ut-tplink.json
 
 # TUe trace
 python3 -m mudscope mudgen \
-    --mudgen_config config/mudgen/tue-tplink.json
+    --config config/mudgen/tue-tplink.json
 
 # MUDgee stores its profiles in the result/ directory by default.
 # We move everything to a separate mud_profiles folder within this 
@@ -33,26 +33,26 @@ mv result/ut-tplink-plug result/mud_profiles/
 # 2. Filter rejected traffic from malign pcap files
 # UT traces
 python3 -m mudscope reject \
-    --reject_mud_rules result/mud_profiles/ut-tplink-plug/ut-tplink-plugrule.csv \
-    --reject_config config/reject/ut-tplink/*.json \
-    --reject_to_named_dir result/rejected/
+    --config config/reject/ut-tplink/*.json \
+    --rules result/mud_profiles/ut-tplink-plug/ut-tplink-plugrule.csv \
+    --output result/rejected/
 
 # TUe traces
 python3 -m mudscope reject \
-    --reject_mud_rules result/mud_profiles/tue-tplink-plug/tue-tplink-plugrule.csv \
-    --reject_config config/reject/tue-tplink/*.json \
-    --reject_to_named_dir result/rejected/
+    --config config/reject/tue-tplink/*.json \
+    --rules result/mud_profiles/tue-tplink-plug/tue-tplink-plugrule.csv \
+    --output result/rejected/
 
 # 3. Transform MUD-rejected traffic (MRT) pcaps into netflows
 # UT traces
 python3 -m mudscope flows_gen \
-    --flowsgen_tgt_dir result/rejected/ut-tplink/ \
-    --flowsgen_outdir result/netflows/rejected/ut-tplink/
+    --input result/rejected/ut-tplink/ \
+    --output result/netflows/rejected/ut-tplink/
 
 # TUe traces
 python3 -m mudscope flows_gen \
-    --flowsgen_tgt_dir result/rejected/tue-tplink/ \
-    --flowsgen_outdir result/netflows/rejected/tue-tplink/
+    --input result/rejected/tue-tplink/ \
+    --output result/netflows/rejected/tue-tplink/
 
 # 3a. Transform benign pcaps into netflows
 python3 -m mudscope.device_mrt_pcaps_to_csv \
@@ -67,31 +67,31 @@ python3 -m mudscope.scale_reference_df_script \
 # 4. Analyze NetFlows to perform clustering
 # UT traces
 python3 -m mudscope analyze \
-    --analysis_action mrta_characterize \
-    --analysis_tgt result/netflows/rejected/ut-tplink/*-CLN.csv \
-    --analysis_capture_metadata config/characterization/ut_tplink.json \
-    --dsr_path result/netflows/benign/benign-custom-format-CLN-SCALED.csv \
-    --analysis_output result/characterization/ut-tplink/
+    --action mrta_characterize \
+    --input result/netflows/rejected/ut-tplink/*-CLN.csv \
+    --metadata config/characterization/ut_tplink.json \
+    --dsr result/netflows/benign/benign-custom-format-CLN-SCALED.csv \
+    --output result/characterization/ut-tplink/
 
 # TUe traces
 python3 -m mudscope analyze \
-    --analysis_action mrta_characterize \
-    --analysis_tgt result/netflows/rejected/tue-tplink/*-CLN.csv \
-    --analysis_capture_metadata config/characterization/tue_tplink.json \
-    --dsr_path result/netflows/benign/benign-custom-format-CLN-SCALED.csv \
-    --analysis_output result/characterization/tue-tplink/
+    --action mrta_characterize \
+    --input result/netflows/rejected/tue-tplink/*-CLN.csv \
+    --metadata config/characterization/tue_tplink.json \
+    --dsr result/netflows/benign/benign-custom-format-CLN-SCALED.csv \
+    --output result/characterization/tue-tplink/
 
 
 # 5. Analyze characterizations to generate MRT feeds
 python3 -m mudscope analyze \
-    --analysis_action device_mrt_evolution_datagen \
-    --analysis_tgt result/characterization/ut-tplink/*.json \
-    --analysis_output result/evolution/ut-tplink.csv
+    --action device_mrt_evolution_datagen \
+    --input result/characterization/ut-tplink/*.json \
+    --output result/evolution/ut-tplink.csv
 
 python3 -m mudscope analyze \
-    --analysis_action device_mrt_evolution_datagen \
-    --analysis_tgt result/characterization/tue-tplink/*.json \
-    --analysis_output result/evolution/tue-tplink.csv
+    --action device_mrt_evolution_datagen \
+    --input result/characterization/tue-tplink/*.json \
+    --output result/evolution/tue-tplink.csv
 
 # python3 -m mudscope.MRTADashboard \
 #     demo \
